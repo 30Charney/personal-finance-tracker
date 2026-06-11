2d3b450f05d57612f5288da21c7011647ebe39d5
Juan Magno Charney <70646915+30Charney@users.noreply.github.com>
2026-06-11 10:05:03 -0300

    Adicionar módulo de receitas

---
+++ income.py
@@ -0,0 +1,9 @@
+receitas = []
+
+def adicionar_receita(descricao, valor):
+    receitas.append({
+        "descricao": descricao,
+        "valor": valor
+    })
+
+print("Módulo de receitas carregado.")
